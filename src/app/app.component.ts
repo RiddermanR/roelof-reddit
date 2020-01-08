6187aa205c20f2c3ef68750ea5d4484608c80e53
RiddermanR <roelof@ridderman.nl>
2020-01-08 16:15:32 +0100

    Moved properties of article to constructor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,7 +23,7 @@ export class AppComponent  {
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
     console.log(`Adding article title: ${title.value} and link: ${link.value}`);
 
-    this.articles.push(new Article(title.value, link.value));
+    this.articles.push(new Article(title.value, link.value, 1));
     title.value = '';
     link.value = '';
     return false;
--- src/app/article/article.model.ts
+++ src/app/article/article.model.ts
@@ -1,12 +1,9 @@
 export class Article {
-  title: string;
-  link: string;
-  votes: number;
 
-  constructor(title: string, link: string, votes?:number) {
-    this.title = title;
-    this.link = link;
-    this.votes = votes || 0;
+  constructor(
+      public title: string, 
+      public link: string, 
+      public votes?: number) {
   }
 
   voteUp() {
